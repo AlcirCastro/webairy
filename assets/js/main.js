a555e6ae79c845d66853f38bba4770ba442dba3f
maria <mariamsaldanha3@gmail.com>
2024-10-07 21:32:18 -0400

    teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,8 @@ const removeLoading = () =>{
 const handleSubmit = (event) => {
     event.preventDefault();
     addLoading();
+    
+    const form = document.querySelector('form');
     const name = document.querySelector('input[name="name"]').value;
     const email = document.querySelector('input[name="email"]').value;
     const check = document.getElementById('check').checked;
